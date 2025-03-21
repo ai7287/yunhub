bab8e2a7772321ed26701923ffba6c1fd2d5bfa3
ai7287 <134990476+ai7287@users.noreply.github.com>
2025-03-21 10:38:19 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,10 @@
+def solution(price):
+    if price >= 500000:
+        price *= 0.8
+    elif price >= 300000:
+        price *= 0.9
+    elif price >= 100000:
+        price *= 0.95
+    else:
+        return int(price)
+    return int(price)
